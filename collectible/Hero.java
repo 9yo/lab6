5748693d0e60d17d456e57625610c0b80c82b02b
artemgut <abogdaov@gmail.com>
2019-03-23 16:33:28 +0300

    hero's multi-thread protection

--- collectible/Hero.java
+++ collectible/Hero.java
@@ -5,6 +5,7 @@ public class Hero implements Comparable<Hero> {
 	protected String name; //name 
 	protected Integer age; // size analog
 	protected String description; // object description
+	protected Location location; // object location
 
 	public Hero() {}
 
@@ -19,13 +20,55 @@ public class Hero implements Comparable<Hero> {
 		this.age = age;
 	}
 
-	public void set_age(Integer age) {
+	public Hero(String name, Integer age, String description, Location location) {
+
+		this.name = name;
+		this.age = age;
+		this.description = description;
+		this.location = location;
+	}
+
+	public void synchronized set_age(Integer age) {
 		this.age = age;
 	}
 
-	public Integer get_age() {
+	public Integer synchronized get_age() {
 		return this.age;
 	}
+
+	public void synchronized set_name(String name) {
+		this.name = name;
+	}
+
+	public String synchronized get_name() {
+		return this.name;
+	}
+
+	public void synchronized set_description(String description) {
+		this.description = description;
+	}
+
+	public String synchronized get_description() {
+		return this.description;
+	}
+
+	public void synchronized set_location(Location location) {
+		this.location = location;
+	}
+
+	public Location synchronized get_location() {
+		return this.location;
+	}
+
+	public int synchronized compareTo(Hero hero) {
+
+		return age.compareTo(hero.get_age());
+	}
+
+}
+
+protected class Location {
+	protected String name;
 
 	public void set_name(String name) {
 		this.name = name;
@@ -34,12 +77,7 @@ public class Hero implements Comparable<Hero> {
 	public String get_name() {
 		return this.name;
 	}
-
-	public int compareTo(Hero hero) {
-
-		return age.compareTo(hero.get_age());
-	}
-
 }
+	
 
 	
